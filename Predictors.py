3aaf91327efcc5f87a97955739f11e2c991c642f
Axel Wickman <axelwickm@gmail.com>
2019-10-09 14:57:17 -0300

    Added predictors along with visdom visualization

---
+++ Predictors.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print("PyTorch running on "+str(device))
+
+class Predictor:
+    def train(self, input, output):
+        return
+
+    def predict(self, input):
+        pass
+
+
+class Baseline(Predictor):
+    def __init__(self, intputCount, outputCount):
+        self.inputCount = intputCount
+        self.outputCount = outputCount
+
+    def predict(self, input):
+        # Predict using last known value
+        return np.full(self.outputCount, np.mean(input))
+
+
+class Encoder(Predictor):
+    class Network(nn.Module):
+        def __init__(self, intputCount, outputCount):
+            super(Encoder.Network, self).__init__()
+            self.fc1 = nn.Linear(intputCount, 50)
+            self.relu1 = nn.ReLU()
+            self.fc2 = nn.Linear(50, 30)
+            self.relu2 = nn.ReLU()
+            self.fc3 = nn.Linear(30, outputCount)
+            self.sigmoid3 = nn.Sigmoid()
+
+        def forward(self, x):
+            out = self.fc1(x)
+            out = self.relu1(out)
+            out = self.fc2(out)
+            out = self.relu2(out)
+            out = self.fc3(out)
+            out = self.sigmoid3(out)
+            return out
+
+    def __init__(self, intputCount, outputCount, LossFunction):
+        self.net = Encoder.Network(intputCount, outputCount)
+        self.net.to(device)
+
+        self.criterion = LossFunction
+        self.optimizer = torch.optim.Adam(self.net.parameters(), lr=0.00003)
+
+    def train(self, input, output):
+        input = torch.from_numpy(input).float()
+        input = torch.autograd.Variable(input)
+        input.to(device)
+
+        output = torch.from_numpy(output).float()
+        output = torch.autograd.Variable(output)
+        output.to(device)
+
+        self.optimizer.zero_grad()
+        netOut = self.net(input)
+
+        loss_size = self.criterion(netOut, output)
+        loss_size.backward()
+        self.optimizer.step()
+        loss_size_detached = loss_size.item()
+
+        return loss_size_detached
+
+    def predict(self, input):
+        input = torch.from_numpy(input).float()
+        input.to(device)
+        netOut = self.net(input)
+        return netOut.detach().numpy()
--- Train.py
+++ Train.py
@@ -1,14 +1,31 @@
 import csv
 from random import sample
+
 import numpy as np
+from visdom import Visdom
 
 import torch
+import torch.nn as nn
 
-TrainingSetPercentage = 0.85
+import Predictors
+
+
 LookBack = 8
 LookForward = 8
 
+TrainingSetPercentage = 0.85
+TrainingEpochs = 30
+AverageEvery = 500
+ShouldPlot = True
+LossFunction = nn.SmoothL1Loss()
+
+PredictorList = [
+    Predictors.Baseline(LookBack, LookForward,),
+    Predictors.Encoder(LookBack, LookForward, LossFunction)
+]
+
 # Read the data from the csv-file into variables
+print("Loading data")
 with open("PrecosAjustados.csv") as csvFile:
     reader = csv.reader(csvFile, delimiter=";")
     headers = next(reader)
@@ -17,14 +34,15 @@ with open("PrecosAjustados.csv") as csvFile:
 
 
 date = data[0]
-IBOV = data[1]
-stocks = list(map(lambda x: list(map(lambda y: float(y.replace(",", ".")), x)), data[2:]))
-betas = list(map(lambda x: float(x.replace(",", ".")), rows[-1][2:]))
+IBOV = np.array(data[1])
+stocks = np.array(list(map(lambda x: list(map(lambda y: float(y.replace(",", ".")), x)), data[2:])))
+betas = np.array(list(map(lambda x: float(x.replace(",", ".")), rows[-1][2:])))
 
 # Normalize the data
-
+stocks /= np.max(stocks)
 
 # Split into training set and testing set
+print("Splitting into training and testing set")
 availableData = np.array(list(np.ndindex((len(stocks), len(date)-LookBack-LookForward))))
 availableData[:, 1] += LookBack
 availableData = list(map(tuple, availableData))
@@ -37,11 +55,88 @@ def getHistory(stockIndex, index, steps=5):
     for i in range(steps):
         stepBack = int(pow(i+1, 1.5))
         stockValues.append(0)
-        #print(index-stepBack, index+1)
-        for j in range(index-stepBack, index+1):
+        minIndex = max(0, index-stepBack)
+        for j in range(minIndex, index+1):
             if j < 0:
                 break
-            stockValues[-1] += stocks[stockIndex][j] / (index+1 - (index-stepBack))
-    return stockValues
+            stockValues[-1] += stocks[stockIndex][j] / (index+1 - minIndex)
+    return np.array(stockValues, dtype=np.float)
 
+
+def getFuture(stockIndex, index, steps=5):
+    stockValues = []
+    for i in range(steps):
+        stepForward = int(pow(i+1, 1.5))
+        stockValues.append(0)
+        maxIndex = min(len(stocks[stockIndex])-1, index+stepForward+1)
+        for j in range(index+1, maxIndex):
+            if len(stocks[stockIndex]) <= j:
+                break
+            stockValues[-1] += stocks[stockIndex][j] / (maxIndex - (index+1))
+    return np.array(stockValues, dtype=np.float)
+
+
+#  Train the predictors
+print("Instantiating predictors")
+if ShouldPlot:
+    vis = Visdom()
+    loss_window = vis.line(X=np.zeros((1)),
+                           Y=np.zeros((1)),
+                           )
+
+for predictor in PredictorList:
+    print("Training: "+predictor.__class__.__name__+" predictor")
+    performanceLog = []
+
+    for epoch in range(TrainingEpochs):
+        print("Epoch "+str(epoch)+":")
+        epochAverage = 0
+        smallAverage = 0
+
+        for i in range(len(trainingSet)):
+            indices = trainingSet[i]
+            inputData = getHistory(indices[0], indices[1], steps=LookBack)
+            outputData = getHistory(indices[0], indices[1], steps=LookForward)
+            loss_size = predictor.train(inputData, outputData)
+            if loss_size is None:
+                print("This predictor cannot be trained\n")
+                break
+
+            epochAverage += loss_size / len(trainingSet)
+            smallAverage += loss_size / AverageEvery
+
+            if (i+1) % AverageEvery == 0:
+                performanceLog.append(smallAverage)
+                smallAverage = 0
+
+                if ShouldPlot:
+                    vis.line(
+                        X=np.linspace(0, len(performanceLog)*AverageEvery, len(performanceLog)),
+                        Y=np.array(performanceLog),
+                        win = loss_window,
+                        name=predictor.__class__.__name__,
+                        opts=dict(
+                            xlabel='training steps',
+                            ylabel='loss size',
+                            ytype='log'
+                        )
+                    )
+        else:
+            print(str(epochAverage)+"\n")
+            continue
+        break
+
+# Validate the predictors
+for predictor in PredictorList:
+    print("Validating: " + predictor.__class__.__name__ + " predictor")
+    averageLoss = 0
+    for i in range(len(testingSet)):
+        indices = testingSet[i]
+        inputData = getHistory(indices[0], indices[1], steps=LookBack)
+        outputData = getHistory(indices[0], indices[1], steps=LookForward)
+        result = predictor.predict(inputData)
+        loss = LossFunction(torch.tensor(result, requires_grad=True, dtype=torch.float),
+                            torch.tensor(outputData, requires_grad=True, dtype=torch.float)).detach().numpy()
+        averageLoss += loss / len(testingSet)
+    print("Average "+LossFunction.__class__.__name__+" performance: "+str(averageLoss)+"\n")
 
